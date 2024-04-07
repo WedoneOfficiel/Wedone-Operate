709cf61ba300bc3373bdb3729fc38df804e5aa17
Wedone <110472725+WedoneOfficiel@users.noreply.github.com>
2024-04-07 12:45:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import sys
 import random
 import requests
 import webbrowser
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QLineEdit, QMessageBox, QInputDialog, QDesktopWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QLineEdit, QDialog, QTextEdit, QDesktopWidget
 from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSlot
 
 class WedoneOperateApp(QWidget):
     def __init__(self):
@@ -18,7 +18,7 @@ class WedoneOperateApp(QWidget):
         self.layout = QVBoxLayout(self)
         self.layout.setContentsMargins(10, 10, 10, 10)
 
-        self.label = QLabel("Bienvenue dans Wedone Operate 3.3 ! Bonne aventure !", self)
+        self.label = QLabel("Bienvenue dans Wedone Operate 3.4 ! Bonne aventure !", self)
         self.label.setStyleSheet("font-size: 16px; font-weight: bold; color: #2b2b2b;")
         self.layout.addWidget(self.label)
 
@@ -44,31 +44,46 @@ class WedoneOperateApp(QWidget):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+    @pyqtSlot()
     def start_game(self):
+        self.execute_game()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Return or event.key() == Qt.Key_Enter:
+            self.execute_game()
+
+    def execute_game(self):
         try:
-            nombre_epreuves = int(self.input.text())
-            if nombre_epreuves <= 0:
-                raise ValueError
+            nombre_epreuves = self.input.text()
+            if not nombre_epreuves.isdigit() or int(nombre_epreuves) <= 0:
+                raise ValueError("Veuillez entrer un nombre valide et supérieur à 0 pour le nombre d'épreuves.")
+
+            nombre_epreuves = int(nombre_epreuves)
 
             score = 0
             for i in range(1, nombre_epreuves + 1):
                 operation, nombre1, nombre2, operation_str, resultat = self.generate_operation()
                 
-                user_input, ok_pressed = QInputDialog.getInt(self, f"Épreuve {i}", f"{nombre1} {operation_str} {nombre2} = ?")
-                if not ok_pressed:
+                dialog = AnswerDialog(f"Épreuve {i}", f"{nombre1} {operation_str} {nombre2} = ?")
+                user_input = dialog.get_answer()
+                if user_input is None:
                     return
 
                 if user_input == resultat:
                     score += 1
-                    QMessageBox.information(self, "Bonne réponse", "Bonne réponse !")
+                    message_window = MessageWindow("Bonne réponse", "Bonne réponse !")
+                    message_window.exec_()
                 else:
-                    QMessageBox.information(self, "Mauvaise réponse", f"Mauvaise réponse. La réponse correcte était {resultat}.")
+                    message_window = MessageWindow("Mauvaise réponse", f"Mauvaise réponse. La réponse correcte était {resultat}.")
+                    message_window.exec_()
 
             pourcentage = score * 100 / nombre_epreuves
             message = f"Vous avez réussi {score} épreuves sur {nombre_epreuves}.\nVotre pourcentage de réussite est de {pourcentage:.2f}%."
-            QMessageBox.information(self, "Résultat", message)
-        except ValueError:
-            QMessageBox.critical(self, "Erreur", "Veuillez entrer un nombre valide et supérieur à 0 pour le nombre d'épreuves.")
+            result_window = MessageWindow("Résultat", message)
+            result_window.exec_()
+        except ValueError as e:
+            error_window = InputErrorWindow("Erreur", str(e))
+            error_window.exec_()
 
     def generate_operation(self):
         operation = random.randint(1, 4)
@@ -109,27 +124,111 @@ class WedoneOperateApp(QWidget):
 
         return operation, nombre1, nombre2, operation_str, resultat
 
-class UpdateMessageBox(QMessageBox):
-    def __init__(self):
+class MessageWindow(QDialog):
+    def __init__(self, title, message):
         super().__init__()
+        self.setWindowTitle(title)
         self.setWindowIcon(QIcon("icon.png"))
-        self.setIcon(QMessageBox.Information)
-        self.setWindowTitle("Mise à jour disponible")
-        self.setTextFormat(Qt.PlainText)
         self.setStyleSheet("background-color: white; color: #2b2b2b;")
 
-    def show_update_message(self, latest_version):
-        message = f"Une nouvelle version de Wedone Operate est disponible (version {latest_version.strip()}) !\n\n"
-        message += "Nous vous recommandons de mettre à jour l'application pour obtenir les dernières fonctionnalités et correctifs de sécurité."
+        layout = QVBoxLayout(self)
 
-        self.setText(message)
-        self.setFont(QFont("Arial", 12))  # Police normale
+        label = QLabel(message)
+        label.setStyleSheet("font-size: 14px;")
+        layout.addWidget(label)
 
-        download_button = self.addButton("  Télécharger  ", QMessageBox.AcceptRole)
+        ok_button = QPushButton("OK")
+        ok_button.clicked.connect(self.accept)
+        ok_button.setStyleSheet("font-size: 14px; background-color: #0672BC; color: white; font-weight: bold; border: 1px solid #0672BC; border-radius: 5px;")
+        layout.addWidget(ok_button)
+
+        self.setLayout(layout)
+
+class InputErrorWindow(QDialog):
+    def __init__(self, title, message):
+        super().__init__()
+        self.setWindowTitle(title)
+        self.setWindowIcon(QIcon("icon.png"))
+        self.setStyleSheet("background-color: white; color: #2b2b2b;")
+
+        layout = QVBoxLayout(self)
+
+        label = QLabel(message)
+        label.setStyleSheet("font-size: 14px;")
+        layout.addWidget(label)
+
+        ok_button = QPushButton("OK")
+        ok_button.clicked.connect(self.accept)
+        ok_button.setStyleSheet("font-size: 14px; background-color: #0672BC; color: white; font-weight: bold; border: 1px solid #0672BC; border-radius: 5px;")
+        layout.addWidget(ok_button)
+
+        self.setLayout(layout)
+
+class AnswerDialog(QDialog):
+    def __init__(self, title, message):
+        super().__init__()
+        self.setWindowTitle(title)
+        self.setWindowIcon(QIcon("icon.png"))
+        self.setStyleSheet("background-color: white; color: #2b2b2b;")
+
+        layout = QVBoxLayout(self)
+
+        label = QLabel(message)
+        label.setStyleSheet("font-size: 14px;")
+        layout.addWidget(label)
+
+        self.answer_input = QLineEdit()
+        self.answer_input.setStyleSheet("font-size: 14px; background-color: #f0f0f0; color: #2b2b2b; border: 1px solid #0672BC; border-radius: 5px;")
+        layout.addWidget(self.answer_input)
+
+        ok_button = QPushButton("OK")
+        ok_button.clicked.connect(self.accept)
+        ok_button.setStyleSheet("font-size: 14px; background-color: #0672BC; color: white; font-weight: bold; border: 1px solid #0672BC; border-radius: 5px;")
+        layout.addWidget(ok_button)
+
+        self.setLayout(layout)
+
+    def get_answer(self):
+        if self.exec_() == QDialog.Accepted:
+            user_input = self.answer_input.text()
+            if user_input.strip() == "":
+                return None
+            return int(user_input)
+        return None
+
+class UpdateWindow(QDialog):
+    def __init__(self, latest_version):
+        super().__init__()
+        self.setWindowTitle("Mise à jour disponible")
+        self.setWindowIcon(QIcon("icon.png"))
+        self.setStyleSheet("background-color: white; color: #2b2b2b;")
+
+        layout = QVBoxLayout(self)
+
+        label = QLabel(f"Une nouvelle version de Wedone Operate est disponible (version {latest_version}) !\n\nNous vous recommandons de mettre à jour l'application pour obtenir les dernières fonctionnalités et correctifs de sécurité.")
+        label.setStyleSheet("font-size: 14px;")
+        layout.addWidget(label)
+
+        buttons_layout = QHBoxLayout()
+        layout.addLayout(buttons_layout)
+
+        # Ajout d'un espace blanc entre les boutons Télécharger et Ignorer
+        buttons_layout.addStretch()
+
+        ignore_button = QPushButton(" Ignorer ")
+        ignore_button.clicked.connect(self.close)
+        ignore_button.setStyleSheet("font-size: 14px; background-color: #E7E3E3; color: #2b2b2b; font-weight: bold; border: 1px solid #E7E3E3; border-radius: 5px;")
+        buttons_layout.addWidget(ignore_button)
+
+        download_button = QPushButton(" Télécharger ")
+        download_button.clicked.connect(self.download_update)
         download_button.setStyleSheet("font-size: 14px; background-color: #0672BC; color: white; font-weight: bold; border: 1px solid #0672BC; border-radius: 5px;")
+        buttons_layout.addWidget(download_button)
 
-        ignore_button = self.addButton("  Ignorer  ", QMessageBox.RejectRole)
-        ignore_button.setStyleSheet("font-size: 14px; background-color: white; color: #3498db; font-weight: bold; border: 1px solid #3498db; border-radius: 5px;")
+        self.setLayout(layout)
+
+    def download_update(self):
+        webbrowser.open("https://github.com/WedoneOfficiel/Wedone-Operate/releases")
 
 def check_for_updates():
     version_url = "https://wedoneofficiel.github.io/Boot-projets-Wedone-Officiel/Wedone-Logiciels-Versions/version-wedone-operate.txt"
@@ -138,13 +237,9 @@ def check_for_updates():
         response = requests.get(version_url)
         if response.status_code == 200:
             latest_version = response.text.strip()
-            if float(latest_version) > 3.3:  # Comparaison en tant que nombre flottant
-                update_box = UpdateMessageBox()
-                update_box.show_update_message(latest_version)
-
-                result = update_box.exec_()
-                if result == QMessageBox.AcceptRole:
-                    webbrowser.open("https://github.com/WedoneOfficiel/Wedone-Operate/releases")
+            if float(latest_version) > 3.4:  # Comparaison en tant que nombre flottant
+                update_window = UpdateWindow(latest_version)
+                update_window.exec_()
     except requests.exceptions.RequestException:
         print("Impossible de récupérer la version la plus récente.")
 
